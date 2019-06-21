c8462336f75d3ff2ffc4adacd38fbbd5136a6b00
Dayu Liu <36178461+LDY681@users.noreply.github.com>
2019-06-21 14:30:07 +0800

    改动
    blah blah blah

--- js/navSideBar.js
+++ js/navSideBar.js
@@ -1,43 +1,32 @@
 //为navSideBar提供支持
-
-// 小屏幕的导航栏 展开或隐藏
-// function navBar_small_show() {
-//   var x = document.getElementById("navBar_small");
-//
-//   if (x.className.indexOf("w3-show") == -1) {
-//     x.className += " w3-show";
-//   } else {
-//     x.className = x.className.replace(" w3-show", "");
-//   }
-// }
-// function navBar_small_close() {
-//   var x = document.getElementById("navBar_small");
-//   if (x.className.indexOf("w3-show") != -1) {
-//     x.className = x.className.replace(" w3-show", "");
-//   }
-// }
-
 //侧边栏 展开或隐藏,展开时修改topNav的左margin为0
 function w3_open() {
-  // 调整正文内容
-  document.getElementById("customContent").style.marginLeft = "200px";
-
-  //调整导航侧边栏
-  document.getElementById("main").style.marginLeft = "180px";
-  document.getElementById("mySidebar").style.width = "196px";
-  document.getElementById("mySidebar").style.display = "block";
-  document.getElementById("openNav").style.display = 'none';
-  var x = document.getElementById("topNav");
-  x.style.marginLeft = "";
-}
-function w3_close() {
     // 调整正文内容
-    document.getElementById("customContent").style.marginLeft = "0";
+    var custom = document.getElementById("customContent");
+    if (custom) {
+        custom.style.marginLeft = "200px";
+    }
 
     //调整导航侧边栏
-  document.getElementById("main").style.marginLeft = "0";
-  document.getElementById("mySidebar").style.display = "none";
-  document.getElementById("openNav").style.display = "inline-block";
-	var x = document.getElementById("topNav");
-	x.style.marginLeft = "38px";
+    document.getElementById("main").style.marginLeft = "180px";
+    document.getElementById("mySidebar").style.width = "196px";
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("openNav").style.display = 'none';
+    var x = document.getElementById("topNav");
+    x.style.marginLeft = "";
+}
+
+function w3_close() {
+    //调整正文内容
+    var custom = document.getElementById("customContent");
+    if (custom) {
+        custom.style.marginLeft = "0";
+    }
+
+    //调整导航侧边栏
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("openNav").style.display = "inline-block";
+    var x = document.getElementById("topNav");
+    x.style.marginLeft = "38px";
 }
