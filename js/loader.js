c064d72d52c6af4d668744de30686b47818435fc
Dayu Liu <36178461+LDY681@users.noreply.github.com>
2019-06-27 18:01:50 +0800

    123
    123

--- js/loader.js
+++ js/loader.js
@@ -1,10 +1,4 @@
 function loader() {
-    var myVar;
-    myVar = setTimeout(showPage, 3000);
-}
-
-function showPage() {
     document.getElementById("loader").style.display = "none";
     document.getElementById("afterLoading").style.display = "block";
-    document.body.style.backgroundImage = 'url("../img/background.jpeg")';
 }
