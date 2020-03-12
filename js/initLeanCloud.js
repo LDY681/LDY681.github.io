20f43e231af2d5b8d357ab9ff4ac63d4b135ac13
LDY681 <ldydyx@gmail.com>
2020-03-11 21:03:30 -0400

    新增聊天室
    侧边栏新增聊天室

--- js/initLeanCloud.js
+++ js/initLeanCloud.js
@@ -1,11 +1,14 @@
 // 每跳转一个单页都要initialize一下，由templateXXX.html调用，嵌入的html不能调用，不然会报错
 
 var { Query, User } = AV;
+var { Realtime, TextMessage } = AV;
 var APP_ID = 'GWIBfipLqh868acSJVJFbl1q-MdYXbMMI';
-var APP_KEY = 'pFE0XYlY4QlTDwlrbbL4IQIY';
+var APP_KEY = 'pFE0XYlY4QlTDwlrbbL4IQIY'
+var server = 'https://jvchwdgn.lc-cn-n1-shared.com';
 
 AV.init({
     appId: APP_ID,
-    appKey: APP_KEY
+    appKey: APP_KEY,
+    serverUrls: server
 });
 
